654f80e1d3fa7cb362ba19f4d759e19641937094
bigo <bigodines@gmail.com>
2019-06-15 18:30:27 -0700

    add payload struct. tests failing

--- lib/slack.go
+++ lib/slack.go
@@ -61,7 +61,7 @@ func (s *SlackSvc) Report(results []Record) error {
 	// TODO: wg.add()
 	reminders := s.GetMessages()
 
-	relevant := make([]*Entry, 0)
+	relevant := make([]Entry, 0)
 	// filter relevant Pull Requests
 	for _, v := range results {
 		pr := v.Node.PullRequest
@@ -72,7 +72,7 @@ func (s *SlackSvc) Report(results []Record) error {
 		}
 		// create and add a report entry
 		l := PrintableLabels(pr.Labels)
-		relevant = append(relevant, &Entry{
+		relevant = append(relevant, Entry{
 			Title:      pr.Title,
 			Author:     pr.Author.Login,
 			Permalink:  pr.Permalink,
@@ -87,17 +87,15 @@ func (s *SlackSvc) Report(results []Record) error {
 		return relevant[i].UpdatedAt.Before(relevant[j].UpdatedAt)
 	})
 
-	// Create Report
-	prs := make([]string, 0)
-	for _, entry := range relevant {
-		line := entry.ToString()
-		if len(line) > 0 {
-			prs = append(prs, line)
-		}
+	report := ReportPayload{
+		ChannelID: s.channelID,
+		Datetime:  time.Now(),
+		PRs:       relevant,
+		Reminders: reminders,
 	}
 
-	log.Debug().Msgf("%+v", prs)
-	err := s.SendMessage(reminders, prs)
+	log.Debug().Msgf("%+v", report)
+	err := s.SendMessage(report)
 	if err != nil {
 		return err
 	}
@@ -105,38 +103,17 @@ func (s *SlackSvc) Report(results []Record) error {
 	return nil
 }
 
-// GetMessages return active countdowns in a report friendly format
-func (s *SlackSvc) GetMessages() []string {
-	msgs := make([]string, 0)
-	if len(s.countdown) < 1 {
-		return msgs
-	}
-	// append countdowns in the future
-	for k, v := range s.countdown {
-		d, err := time.Parse(layoutISO, k)
-		if err != nil {
-			continue
-		}
-		daysUntil := int64(time.Until(d).Hours() / 24)
-		if daysUntil > 0 {
-			msgs = append(msgs, fmt.Sprintf("Friendly Reminder: \"%s\" is *%d* days away!", v, daysUntil))
-		}
-	}
-
-	return msgs
-}
-
 // Send individual slack message to configured slack channel
-func (s *SlackSvc) SendMessage(reminders, prs []string) error {
+func (s *SlackSvc) SendMessage(report ReportPayload) error {
 	attachments := make([]map[string]interface{}, 1)
-	attachments[0] = map[string]interface{}{"text": fmt.Sprintf("Howdy! Here's a list of *%d* PRs waiting to be reviewed and merged:", len(prs))}
-	for _, v := range prs {
-		attachments = append(attachments, map[string]interface{}{"text": v})
+	attachments[0] = map[string]interface{}{"text": fmt.Sprintf("Howdy! Here's a list of *%d* PRs waiting to be reviewed and merged:", len(report.PRs))}
+	for _, v := range report.PRs {
+		attachments = append(attachments, map[string]interface{}{"text": v.ToString()})
 	}
 
-	if len(reminders) > 0 {
-		for _, v := range reminders {
-			attachments = append(attachments, map[string]interface{}{"text": v})
+	if len(report.Reminders) > 0 {
+		for _, v := range report.Reminders {
+			attachments = append(attachments, map[string]interface{}{"text": v.Text})
 		}
 	}
 
@@ -163,6 +140,30 @@ func (s *SlackSvc) SendMessage(reminders, prs []string) error {
 
 	log.Info().Msgf("Message successfully sent to channel %s", s.channelID)
 	return nil
+}
+
+// GetMessages return active countdowns in a report friendly format
+func (s *SlackSvc) GetMessages() []Reminder {
+	msgs := make([]Reminder, 0)
+	if len(s.countdown) < 1 {
+		return msgs
+	}
+	// append countdowns in the future
+	for k, v := range s.countdown {
+		d, err := time.Parse(layoutISO, k)
+		if err != nil {
+			continue
+		}
+		daysUntil := int64(time.Until(d).Hours() / 24)
+		if daysUntil > 0 {
+			msgs = append(msgs, Reminder{
+				Date: d,
+				Text: fmt.Sprintf("Friendly Reminder: \"%s\" is *%d* days away!", v, daysUntil),
+			})
+		}
+	}
+
+	return msgs
 }
 
 // Printable format of an Entry
